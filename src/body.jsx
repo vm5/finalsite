ebb89ffbfd3d08595951ad9f5686ac26073b240d
vm5 <62893030+vm5@users.noreply.github.com>
2024-08-12 21:12:55 +0530

    Update body.jsx

--- src/body.jsx
+++ src/body.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import './App.css';
 
 const DummyAlumni = [
-  { company: 'Apple', designation: 'Software Development Engineer', logo: '/apple-removebg-preview.png' },
   { company: 'Walmart', designation: 'Software Development Engineer', logo: '/walmart.png' },
   { company: 'Morgan Stanley', designation: 'Tech Analyst', logo: '/morgan-stanley.png' },
+   { company: 'Apple', designation: 'Software Development Engineer', logo: '/apple-removebg-preview.png' },
   { company: 'CRED', designation: 'Software Development Engineer', logo: '/cred.png' },
   { company: 'Deloitte', designation: 'Solution Delivery Analyst', logo: '/deloitte.png' },
   { company: 'HCL', designation: 'Product Manager', logo: '/hcl.png' },
