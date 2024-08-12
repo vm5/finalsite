b97b0e989042ceea01205f9840546ac8200cab2c
vm5 <62893030+vm5@users.noreply.github.com>
2024-08-12 14:04:43 +0530

    Update body.jsx

--- src/body.jsx
+++ src/body.jsx
@@ -233,29 +233,29 @@ const styles = {
   companyContainer: {
     overflow: 'hidden',
     position: 'relative',
-    height: '4rem', // Adjust based on content height
+    height: '4rem', 
     backgroundColor: 'LightCyan',
     borderRadius: '0.5rem',
     boxShadow: '0 0.5rem 1rem rgba(0, 0, 0, 0.1)',
     display: 'flex',
     alignItems: 'center',
-    width: '100%', // Ensure the container is wide enough
+    width: '100%', 
   },
   companyList: {
     display: 'flex',
     flexDirection: 'row',
     whiteSpace: 'nowrap',
-    animation: 'slide-left 60s linear infinite', // Adjust the duration here
-    width: '200%', // Ensure the list is wide enough for seamless scrolling
+    animation: 'slide-left 60s linear infinite', 
+    width: '200%', 
   },
   companyItem: {
     display: 'flex',
     alignItems: 'center',
-    padding: '1rem', // Adjust padding for better fit
-    fontSize: '2.2rem', // Adjust font size for better fit
+    padding: '1.3rem',
+    fontSize: '1.9rem', 
   },
   logo: {
-    width: '50px', // Adjust logo size as needed
+    width: '50px', 
     height: 'auto',
     marginRight: '1rem',
   },
