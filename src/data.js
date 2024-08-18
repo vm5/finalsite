2ba1cdc4959c85bbd58fc9f3f7de0b0619a94480
vm5 <62893030+vm5@users.noreply.github.com>
2024-08-18 14:01:41 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const QueryData = {
     "IBM": {
-      codes: ["nucfusibm@2024", "nucfusIBM@2024"], // Valid codes for Company A
+      codes: ["nucfusibm@2024", "nucfusIBM@2024"], 
       queries: [
         "How do I prepare for an interview at IBM?"
       ],
