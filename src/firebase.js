6a6b5801029be00170b9c3d6b133f06b87fa209d
vm5 <62893030+vm5@users.noreply.github.com>
2024-08-18 18:56:18 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,8 @@
 // firebase.js
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
 
+// Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCsx7mCjpBA0lup2-L9Dz2aVph9iuFTw_s",
     authDomain: "nucleusfusion-e3a6f.firebaseapp.com",
@@ -10,9 +11,29 @@ const firebaseConfig = {
     messagingSenderId: "1072587854792",
     appId: "1:1072587854792:web:e4e8a3c6921cbc228e26fd",
     measurementId: "G-GWV49V75P0"
-  };
+};
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-export { auth, GoogleAuthProvider, signInWithPopup, signOut };
+// Set up reCAPTCHA verifier for phone number authentication
+const setupRecaptcha = (containerId) => {
+    return new RecaptchaVerifier(containerId, {
+        'size': 'invisible',
+        'callback': (response) => {
+            // reCAPTCHA solved, allow signInWithPhoneNumber.
+            console.log('reCAPTCHA solved');
+        }
+    }, auth);
+};
+
+// Function to sign in with phone number
+const signInWithPhone = (phoneNumber, recaptchaVerifier) => {
+    return signInWithPhoneNumber(auth, phoneNumber, recaptchaVerifier);
+};
+
+// Export the necessary Firebase Authentication functions and objects
+export { auth, GoogleAuthProvider, signInWithPopup, signOut, setupRecaptcha, signInWithPhone };
